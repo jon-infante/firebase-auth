90fa52438eefb2690933efbf5cd1b20b3014d569
inv0w <jonjoninfante@gmail.com>
2020-04-16 19:26:21 -0700

    user log in

--- scripts/auth.js
+++ scripts/auth.js
@@ -22,5 +22,21 @@ logout.addEventListener('click', (e) => {
     e.preventDefault();
     auth.signOut().then(() => {
         console.log('user signed out')
+    });
+});
+
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user)
+        // close login modal and reset form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
     })
 })
